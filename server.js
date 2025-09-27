c0c3a907759d00dc8a13e9dd8050d5bf18e2e024
greemaltend4 <dengestabiy@hotmail.com>
2025-09-27 12:20:36 +0000

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie'))
+    .catch(err => console.error('Erreur de connexion à MongoDB:', err));
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l API de l Intégration Cloud Gestionale!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
+});
